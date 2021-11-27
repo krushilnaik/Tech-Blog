731cb5a828d6719d39418e94e4bce46141d7c776
Krushil Naik <krushilnaik96@gmail.com>
2021-11-26 19:53:04 -0500

    Add some seeds

---
+++ src/seeds/index.js
@@ -0,0 +1,23 @@
+const seedPosts = require('./post.seed');
+const seedUsers = require('./user.seed');
+const seedComments = require('./comment.seed');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+	await sequelize.sync({ force: true });
+	console.log('\n----- DATABASE SYNCED -----\n');
+
+	await seedUsers();
+	console.log('\n----- USERS SEEDED -----\n');
+
+	await seedPosts();
+	console.log('\n----- POSTS SEEDED -----\n');
+
+	await seedComments();
+	console.log('\n----- COMMENTS SEEDED -----\n');
+
+	process.exit(0);
+};
+
+seedAll();
